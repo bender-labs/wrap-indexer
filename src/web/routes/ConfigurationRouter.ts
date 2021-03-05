299c6d8b5a835519a2ee5f80279bb0db7a142228
Charles Couillard <charles@benderlabs.io>
2021-03-05 14:08:25 +0100

    feat(api endpoints): configuration endpoint

--- src/web/routes/ConfigurationRouter.ts
+++ src/web/routes/ConfigurationRouter.ts
@@ -1,18 +1,27 @@
 import { Request, Response, Router } from 'express';
 import { Dependencies } from '../../bootstrap';
-import Knex from 'knex';
 import { TezosQuorumDao } from '../../dao/TezosQuorumDao';
 import { EthereumQuorumDao } from '../../dao/EthereumQuorumDao';
 
 type Configuration = {
+  ethereumNetwork: string;
+  tezosNetwork: string;
+  ethereumWrapContract: string;
+  tezosMinterContract: string;
+  tezosQuorumContract: string;
   wrapRequiredSignatures: number;
   unwrapRequiredSignatures: number;
 }
 
-async function buildConfiguration(dbClient: Knex): Promise<Configuration> {
-  const tezosThreashold = await new TezosQuorumDao(dbClient).getThreshold();
-  const ethereumThreshold = await new EthereumQuorumDao(dbClient).getThreshold();
+async function buildConfiguration(dependencies: Dependencies): Promise<Configuration> {
+  const tezosThreashold = await new TezosQuorumDao(dependencies.dbClient).getThreshold();
+  const ethereumThreshold = await new EthereumQuorumDao(dependencies.dbClient).getThreshold();
   return {
+    ethereumNetwork: dependencies.configuration.ethereum.currentNetwork,
+    tezosNetwork: dependencies.configuration.tezos.currentNetwork,
+    ethereumWrapContract: dependencies.ethereumConfiguration.wrapContractAddress,
+    tezosMinterContract: dependencies.tezosConfiguration.minterContractAddress,
+    tezosQuorumContract: dependencies.tezosConfiguration.quorumContractAddress,
     wrapRequiredSignatures: tezosThreashold,
     unwrapRequiredSignatures: ethereumThreshold,
   };
@@ -21,7 +30,7 @@ async function buildConfiguration(dbClient: Knex): Promise<Configuration> {
 function build(dependencies: Dependencies): Router {
   const router = Router();
   router.get('/', async (_req: Request, res: Response) => {
-    return res.json(await buildConfiguration(dependencies.dbClient));
+    return res.json(await buildConfiguration(dependencies));
   });
   return router;
 }
