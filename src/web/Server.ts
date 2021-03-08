a097f8e148ac61010176ffd8ac301b828d690da4
Charles Couillard <charles@benderlabs.io>
2021-03-08 15:45:28 +0100

    chore: remove helmet

--- src/web/Server.ts
+++ src/web/Server.ts
@@ -1,11 +1,10 @@
-import helmet from 'helmet';
 import * as express from 'express';
 import { Express, NextFunction, Request, Response } from 'express';
 import BaseRouter from './routes/Router';
 import { Dependencies } from '../bootstrap';
 
 export function httpServer(dependencies: Dependencies): Express {
-  const { logger, configuration } = dependencies;
+  const { logger } = dependencies;
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
@@ -13,9 +12,6 @@ export function httpServer(dependencies: Dependencies): Express {
     logger.info(`${req.method} ${req.url}`);
     next();
   });
-  if (configuration.node.environment === 'production') {
-    app.use(helmet());
-  }
   app.use('/v1', BaseRouter(dependencies));
   return app;
 }
