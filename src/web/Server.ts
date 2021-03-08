844332f9eac5c984e41603eaf6cd9ae6208cfacd
Charles Couillard <charles@benderlabs.io>
2021-03-08 18:19:43 +0100

    chore(api): add cors support

--- src/web/Server.ts
+++ src/web/Server.ts
@@ -1,5 +1,6 @@
 import * as express from 'express';
 import { Express, NextFunction, Request, Response } from 'express';
+import * as cors from 'cors';
 import BaseRouter from './routes/Router';
 import { Dependencies } from '../bootstrap';
 
@@ -8,6 +9,7 @@ export function httpServer(dependencies: Dependencies): Express {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
   app.use((req: Request, _res: Response, next: NextFunction) => {
     logger.info(`${req.method} ${req.url}`);
     next();
