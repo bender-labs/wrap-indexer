3533cb1c84adb7291a05e35225eea91a8bf5d1ac
Charles Couillard <charles@benderlabs.io>
2021-03-12 16:10:22 +0100

    chore: remove http logs

--- src/web/Server.ts
+++ src/web/Server.ts
@@ -1,19 +1,14 @@
 import * as express from 'express';
-import { Express, NextFunction, Request, Response } from 'express';
+import { Express } from 'express';
 import * as cors from 'cors';
 import BaseRouter from './routes/Router';
 import { Dependencies } from '../bootstrap';
 
 export function httpServer(dependencies: Dependencies): Express {
-  const { logger } = dependencies;
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
-  app.use((req: Request, _res: Response, next: NextFunction) => {
-    logger.info(`${req.method} ${req.url}`);
-    next();
-  });
   app.use('/v1', BaseRouter(dependencies));
   return app;
 }
