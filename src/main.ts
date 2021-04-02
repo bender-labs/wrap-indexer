c6d9522fa00a981126410b7f935a936712cc8ffa
Charles Couillard <charles@benderlabs.io>
2021-04-02 14:56:26 +0200

    chore: stop cron jobs on sigterm

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,29 @@ import { bootstrap } from './bootstrap';
 import { scheduleJobs } from './jobs';
 
 const configuration = loadConfiguration();
-const ethereumConfiguration = configuration.ethereum.networks[configuration.ethereum.currentNetwork];
-const tezosConfiguration = configuration.tezos.networks[configuration.tezos.currentNetwork];
-const dependencies = bootstrap(configuration, ethereumConfiguration, tezosConfiguration);
+const ethereumConfiguration =
+  configuration.ethereum.networks[configuration.ethereum.currentNetwork];
+const tezosConfiguration =
+  configuration.tezos.networks[configuration.tezos.currentNetwork];
+const dependencies = bootstrap(
+  configuration,
+  ethereumConfiguration,
+  tezosConfiguration
+);
 
-scheduleJobs(dependencies).start();
+const crontab = scheduleJobs(dependencies);
+crontab.start();
 
-httpServer(dependencies).listen(configuration.http.port, () => {
-  dependencies.logger.info(`Express server started on port: ${configuration.http.port}`);
+const server = httpServer(dependencies).listen(configuration.http.port, () => {
+  dependencies.logger.info(
+    `Express server started on port: ${configuration.http.port}`
+  );
 });
 
-
+process.on('SIGTERM', () => {
+  dependencies.logger.info('Server stopping...');
+  crontab.stop();
+  server.close(() => {
+    process.exit(0);
+  });
+});
