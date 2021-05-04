04d558e137863ddba16501b3156df02796555e1d
Charles Couillard <charles@benderlabs.io>
2021-05-04 11:27:19 +0200

    feat: run one job at a time

--- src/infrastructure/Crontab.ts
+++ src/infrastructure/Crontab.ts
@@ -1,9 +1,31 @@
 import { CronJob } from 'cron';
+import { Dependencies } from '../bootstrap';
+import { Logger } from 'tslog';
 
 export class Crontab {
+  constructor(dependencies: Dependencies) {
+    this._logger = dependencies.logger;
+  }
+
   register(job: () => Promise<void>, pattern: string): void {
+    let taskRunning = false;
     this._jobs.push(
-      new CronJob({ cronTime: pattern, onTick: job, runOnInit: true })
+      new CronJob({
+        cronTime: pattern,
+        onTick: async () => {
+          if (taskRunning) {
+            this._logger.debug('Task already running');
+            return;
+          }
+          taskRunning = true;
+          try {
+            await job();
+          } finally {
+            taskRunning = false;
+          }
+        },
+        runOnInit: true,
+      })
     );
   }
 
@@ -16,4 +38,5 @@ export class Crontab {
   }
 
   private _jobs: CronJob[] = [];
+  private _logger: Logger;
 }
--- src/jobs.ts
+++ src/jobs.ts
@@ -16,16 +16,43 @@ const every10Minutes = '*/10 * * * *';
 const every30Minutes = '*/30 * * * *';
 
 export function scheduleJobs(dependencies: Dependencies): Crontab {
-  const crontab = new Crontab();
-  crontab.register(() => new EthereumInitialWrapIndexer(dependencies).index(), everyMinute);
-  crontab.register(() => new TezosInitialUnwrapIndexer(dependencies).index(), everyMinute);
-  crontab.register(() => new SignatureIndexer(dependencies).index(), everyMinute);
-  crontab.register(() => new EthereumFinalizedUnwrapIndexer(dependencies).index(), everyMinute);
-  crontab.register(() => new TezosFinalizedWrapIndexer(dependencies).index(), everyMinute);
-  crontab.register(() => new TezosQuorumIndexer(dependencies).index(), every10Minutes);
-  crontab.register(() => new EthereumQuorumIndexer(dependencies).index(), every10Minutes);
-  crontab.register(() => new TokensIndexer(dependencies).index(), every10Minutes);
+  const crontab = new Crontab(dependencies);
+  crontab.register(
+    () => new EthereumInitialWrapIndexer(dependencies).index(),
+    everyMinute
+  );
+  crontab.register(
+    () => new TezosInitialUnwrapIndexer(dependencies).index(),
+    everyMinute
+  );
+  crontab.register(
+    () => new SignatureIndexer(dependencies).index(),
+    everyMinute
+  );
+  crontab.register(
+    () => new EthereumFinalizedUnwrapIndexer(dependencies).index(),
+    everyMinute
+  );
+  crontab.register(
+    () => new TezosFinalizedWrapIndexer(dependencies).index(),
+    everyMinute
+  );
+  crontab.register(
+    () => new TezosQuorumIndexer(dependencies).index(),
+    every10Minutes
+  );
+  crontab.register(
+    () => new EthereumQuorumIndexer(dependencies).index(),
+    every10Minutes
+  );
+  crontab.register(
+    () => new TokensIndexer(dependencies).index(),
+    every10Minutes
+  );
   crontab.register(() => new FeesIndexer(dependencies).index(), every30Minutes);
-  crontab.register(() => new SignaturePinningService(dependencies).index(), every30Minutes);
+  crontab.register(
+    () => new SignaturePinningService(dependencies).index(),
+    every30Minutes
+  );
   return crontab;
 }
